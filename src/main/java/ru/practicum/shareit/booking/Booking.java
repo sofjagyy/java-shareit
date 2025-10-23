d877d21bcc903084c84521b95b938ae32ed4998a
sofjagyy <sofjagy@yandex.ru>
2025-10-23 19:00:22 +0200

    убрана избыточная валидация в моделях

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,8 +1,5 @@
 package ru.practicum.shareit.booking;
 
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.FutureOrPresent;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.User;
@@ -12,15 +9,9 @@ import java.time.LocalDateTime;
 @Data
 public class Booking {
     private Long id;
-    @NotNull(message = "Дата начала не может быть null")
-    @FutureOrPresent(message = "Дата начала должна быть в настоящем или будущем")
     private LocalDateTime start;
-    @NotNull(message = "Дата окончания не может быть null")
-    @Future(message = "Дата окончания должна быть в будущем")
     private LocalDateTime end;
-    @NotNull(message = "Вещь не может быть null")
     private Item item;
-    @NotNull(message = "Арендатор не может быть null")
     private User booker;
     private Status status;
 }
